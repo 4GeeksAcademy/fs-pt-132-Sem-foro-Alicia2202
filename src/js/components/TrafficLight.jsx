c1ce5c98bfab8f10d25f520919e2477e4b7b5b74
Alicia2202 <alicialf02@gmail.com>
2026-03-16 20:53:35 +0000

    feat: clases para encendido en index.css

--- src/js/components/TrafficLight.jsx
+++ src/js/components/TrafficLight.jsx
@@ -1,14 +1,23 @@
-import react from "react";
+import react, { useState } from "react";
 
 const TrafficLight = () => {
+
+    const [activeLight, setactiveLight] = useState ('');
+
+    const handleClick = (light) => {
+        setactiveLight(light)
+    }
 
     return (
         <div className="d-flex flex-column align-items-center mt-5">
 
             <div className="trafficlight p-3 d-flex flex-column align-items-center">
-                <div className="lightred rounded-circle"></div>
-                <div className="lightyellow rounded-circle"></div>
-                <div className="lightgreen rounded-circle"></div>
+                <div onClick={() => handleClick('light-red')}
+                className={`light rounded-circle light-red ${activeLight === 'light-red' ? 'shadow-red' : 'opacity-50'} `}></div>
+                <div onClick={() => handleClick('light-yellow')}
+                className={`light rounded-circle light-yellow ${activeLight === 'light-yellow' ? 'shadow-yellow' : 'opacity-50'} `}></div>
+                <div onClick={() => handleClick('light-green')}
+                className={`light rounded-circle light-green ${activeLight === 'light-green' ? 'shadow-green' : 'opacity-50'} `}></div>
             </div>
         </div>
     )
