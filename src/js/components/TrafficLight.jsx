02ec5fde8e09a3ca2a7fec28107129eb4ab5f376
Alicia2202 <alicialf02@gmail.com>
2026-03-18 12:24:34 +0000

    Feat: Añadido un botón en el componente TrafficLight para poner automáticamente el semáforo

--- src/js/components/TrafficLight.jsx
+++ src/js/components/TrafficLight.jsx
@@ -1,24 +1,63 @@
-import react, { useState } from "react";
+import react, { useState, useEffect} from "react";
 
 const TrafficLight = () => {
 
-    const [activeLight, setactiveLight] = useState ('');
+    const [activeLight, setactiveLight] = useState('light-red');
+    const [isAuto, setIsAuto] = useState(false)
+    const colors = ['light-red', 'light-yellow', 'light-green']
 
     const handleClick = (light) => {
         setactiveLight(light)
     }
+
+    useEffect(() => {
+        if (!isAuto) return;
+        const interval = setInterval(() => {
+            setactiveLight(prev => {
+                const currentIndex = colors.indexOf(prev);
+                const nextIndex = (currentIndex + 1) & colors.length;
+                return colors[nextIndex];
+            });
+        }, 1000);
+
+        return () => clearInterval(interval);
+    }, [isAuto]);
+
+    const toggleAuto = () => {
+        setIsAuto(prev => !prev);
+    };
+    // setactiveLight(prev => {
+    //     const currentIndex = colors.indexOf(prev);
+    //     const nextIndex = ((currentIndex) + 1) % colors.length;
+    //     setactiveLight(colors[nextIndex]);
+    // });
+
+
+    // const autoLight = () => {
+    //     if (intervalRef.current) {
+    //         clearInterval(intervalRef.current);
+    //         intervalRef.current = null;
+    //     } else {
+    //         intervalRef.current = setInterval(nextLight, 1000);
+    //     }
+    // }
+
+
+
 
     return (
         <div className="d-flex flex-column align-items-center mt-5">
 
             <div className="trafficlight p-3 d-flex flex-column align-items-center">
                 <div onClick={() => handleClick('light-red')}
-                className={`light rounded-circle light-red ${activeLight === 'light-red' ? 'shadow-red' : 'opacity-50'} `}></div>
+                    className={`light rounded-circle light-red ${activeLight === 'light-red' ? 'shadow-red' : 'opacity-50'} `}></div>
                 <div onClick={() => handleClick('light-yellow')}
-                className={`light rounded-circle light-yellow ${activeLight === 'light-yellow' ? 'shadow-yellow' : 'opacity-50'} `}></div>
+                    className={`light rounded-circle light-yellow ${activeLight === 'light-yellow' ? 'shadow-yellow' : 'opacity-50'} `}></div>
                 <div onClick={() => handleClick('light-green')}
-                className={`light rounded-circle light-green ${activeLight === 'light-green' ? 'shadow-green' : 'opacity-50'} `}></div>
+                    className={`light rounded-circle light-green ${activeLight === 'light-green' ? 'shadow-green' : 'opacity-50'} `}></div>
             </div>
+
+            <button className="btn btn-secondary mt-3" onClick={toggleAuto}>{isAuto ? "Parar Semáforo" : "Activar Semáforo"}</button>
         </div>
     )
 };
